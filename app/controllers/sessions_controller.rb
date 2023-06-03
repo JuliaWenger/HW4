4de4ea2e195e86e278b0997c9547eec813a4f52e
Julia Wenger <julia.wenger@kellogg.northwestern.edu>
2023-06-03 15:07:22 +0000

    Updated Logins

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,8 @@ class SessionsController < ApplicationController
     # 1. try to find the user by their unique identifier
     @user = User.find_by({ "email" => params["email"] })
     # 2. if the user exists -> check if they know their password
-    if @user != nil && @user["password"] == params["password"]
-    #if BCrypt::Password.new(@user["password"]) == params["password"]
+    if @user != nil #&& @user["password"] == params["password"]
+    if BCrypt::Password.new(@user["password"]) == params["password"]
         # 3. if they know their password -> login is successful
 
         #assign a cookie to show the user name in the corner
@@ -20,7 +20,7 @@ class SessionsController < ApplicationController
         # 4. if the email does not exist or they do not know their password -> login fails
         flash["notice"] = "Sorry, please try again"
         redirect_to "/login"
-      #end
+      end
    end
   #end 
   end 
